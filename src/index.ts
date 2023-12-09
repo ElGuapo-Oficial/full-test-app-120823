b84e8588d91e6bee424e2ee13511f305773ec562
Najash Marron <najashmarron@gmail.com>
2023-12-08 22:40:49 -0800

    Adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'https://www.najashmarron.com'
+  }));
 app.use(express.static('public'));
 
 app.use(routes);
