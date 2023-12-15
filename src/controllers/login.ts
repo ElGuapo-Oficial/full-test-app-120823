722afd4781160e6cb8e3cc47d57532ba8f61752f
Najash Marron <najashmarron@gmail.com>
2023-12-14 19:10:58 -0800

    Enhaced Login

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,32 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import { insertUser, searchUser } from '../db/users';
 
+dotenv.config();
 const saltRounds = 10;
-
-const signup = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { email, password } = req.body;
-        console.log("SERVER1: ", email, password);
-
-        const userExist = await searchUser(email);
-        console.log("userExist: ", userExist);
-        if (userExist) {
-            return res.status(409).json({ message: 'User already exists with this email.' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-        console.log("SERVER2: ", email, password);
-        const user = await insertUser(email, hashedPassword);
-        res.json({ message: 'Signup successful', id: user?.id });
-
-    } catch (error) {
-        console.log('Error on Signup: ', error);
-        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-        res.status(500).json({message: `Error on Singup: ${errorMessage}`});
-    }
-}
+const secretKey = process.env.JWT_SECRET!;
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -34,7 +14,8 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
         const user = await searchUser(email);
         if (user && user.password && await bcrypt.compare(password, user.password)) {
-            return res.json({ message: 'Login successful', id: user.id });
+            const token = jwt.sign({ userId: user.id }, secretKey);
+            return res.json({ token, message: 'Login successful', id: user.id });
         }
 
         res.status(401).json({ message: 'Login invalid credentials' });
@@ -43,6 +24,27 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
         console.error('Error on Login: ', error);
         const errorMessage = error instanceof Error ? error.message : 'Unknown error';
         res.status(500).json({ message: `Error on Login: ${errorMessage}` });
+    }
+}
+
+const signup = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+
+        const userExist = await searchUser(email);
+        console.log("userExist: ", userExist);
+        if (userExist) {
+            return res.status(409).json({ message: 'User already exists with this email.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const user = await insertUser(email, hashedPassword);
+        res.json({ message: 'Signup successful', id: user?.id });
+
+    } catch (error) {
+        console.log('Error on Signup: ', error);
+        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+        res.status(500).json({message: `Error on Singup: ${errorMessage}`});
     }
 }
 
